cdd5365d5199649d25bf06e517b9410fe69f7971
June <june.zhao@yahoo.com>
2023-08-04 14:24:10 +0200

    add NavLink animation

--- components/NavLink.js
+++ components/NavLink.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { motion } from "framer-motion";
 
 export function NavLink({ href, name }) {
   const pathname = usePathname();
@@ -9,12 +10,21 @@ export function NavLink({ href, name }) {
     <li>
       <Link
         href={href}
-        className={
-          "flex flex-row items-center font-medium px-3 py-1" +
-          (active ? " bg-black text-white rounded-full" : "")
-        }
+        key={href}
+        className="relative rounded-full px-3 py-1.5 font-medium text-white outline-sky-400 transition focus-visible:outline-2"
+        style={{
+          WebkitTapHighlightColor: "transparent",
+        }}
       >
-        {name}
+        {active && (
+          <motion.span
+            layoutId="bubble"
+            className="absolute inset-0 bg-black "
+            style={{ borderRadius: 9999 }}
+            transition={{ type: "spring", bounce: 0.2, duration: 0.6 }}
+          />
+        )}
+        <span className="relative z-10 mix-blend-exclusion"> {name}</span>
       </Link>
     </li>
   );
