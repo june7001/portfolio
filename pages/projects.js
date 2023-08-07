76dec7fd54711d28913f3de8492e9215ecaacdba
June <june.zhao@yahoo.com>
2023-08-07 21:03:52 +0200

    add space between cards

--- pages/projects.js
+++ pages/projects.js
@@ -4,7 +4,7 @@ import { ProjectCard } from "@/components/ProjectCard";
 export default function Projects() {
   return (
     <div>
-      <div className="grid grid-cols-1 gap-0.5 md:grid-cols-2 lg:grid-cols-3">
+      <div className="grid grid-cols-1 gap-0.5 space-x-4 md:grid-cols-2 lg:grid-cols-3">
         {projects.map((project, index) => (
           <ProjectCard key={index} project={project} />
         ))}
