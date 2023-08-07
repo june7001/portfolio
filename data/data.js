3a820f332561f948eedba647fdfd249cb397d242
June <june.zhao@yahoo.com>
2023-08-07 19:18:41 +0200

    include 'featured' property

--- data/data.js
+++ data/data.js
@@ -1,6 +1,7 @@
 export const projects = [
   {
     title: "Portfolio",
+    featured: true,
     githubLink: "https://github.com/june7001/portfolio",
     img: "/images/ghost.png",
     techStack: ["Next.js", "TailwindCSS", "Framer Motion"],
@@ -9,6 +10,7 @@ export const projects = [
   },
   {
     title: "Juni Blommor",
+    featured: false,
     githubLink: "https://github.com/project-2",
     img: "/images/ghost.png",
     techStack: ["Shopify", "Next.js", "Framer Motion"],
@@ -17,6 +19,7 @@ export const projects = [
   },
   {
     title: "List",
+    featured: true,
     githubLink: "https://github.com/june7001/list",
     img: "/images/ghost.png",
     techStack: ["Express", "MySQL", "Next.js", "Tailwind CSS"],
