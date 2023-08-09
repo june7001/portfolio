e49eeeac4c0fa338c753e348faaf60f2c9db3ae8
June <june.zhao@yahoo.com>
2023-08-09 02:55:08 +0200

    initial contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,3 +1,17 @@
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const handleChange = (e) => {};
+  const handleSubmit = (e) => {};
+
   return <div></div>;
 }
