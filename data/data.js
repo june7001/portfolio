29ec10818eceec18fa638ac04b45afff61208e60
June <june.zhao@yahoo.com>
2023-08-10 23:23:03 +0200

    update data

--- data/data.js
+++ data/data.js
@@ -8,21 +8,4 @@ export const projects = [
     description: "Improvements ongoing",
     link: "https://june.contact",
   },
-  {
-    title: "Juni Blommor",
-    featured: false,
-    githubLink: "https://github.com/june7001/commerce",
-    img: "/images/ghost.png",
-    techStack: ["Shopify", "Next.js", "Framer Motion"],
-    description: "A headless online flower shop",
-    link: "https://june7001.myshopify.com/",
-  },
-  {
-    title: "List",
-    featured: true,
-    githubLink: "https://github.com/june7001/list",
-    img: "/images/ghost.png",
-    techStack: ["Express", "MySQL", "Next.js", "Tailwind CSS"],
-    description: "Fullstack list sharing app",
-  },
 ];
