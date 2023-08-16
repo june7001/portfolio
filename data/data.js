356c4b60766f7cdd318f238c03ce66b9ba1febb3
June <june.zhao@yahoo.com>
2023-08-16 22:43:07 +0200

    update data

--- data/data.js
+++ data/data.js
@@ -8,4 +8,13 @@ export const projects = [
     description: "Improvements ongoing",
     link: "https://june.contact",
   },
+  {
+    title: "Blomsterhandel",
+    featured: true,
+    githubLink: "https://github.com/june7001/commerce",
+    img: "/images/blomsterhandel.png",
+    techStack: ["Next.js(app router)", "Shopify"],
+    description: "A headless shopify flower shop",
+    link: "https://commerce-three-rose-76.vercel.app/",
+  },
 ];
